01bbc6ca084c15ef59e12837fc36e8edbe1a60f4
eb8dev <bhanuprakasheenadula12@outlook.com>
2025-10-06 23:05:32 +0530

    day 2

---
+++ twitterlite/posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='posts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('create/', views.create_post, name='create_post'),
+
+]
--- twitterlite/posts/views.py
+++ twitterlite/posts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Post
+from django.contrib.auth import login
+from .forms import RegisterForm, PostForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'posts/home.html', {'posts': posts})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'posts/register.html', {'form': form})
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'posts/create_post.html', {'form': form})
